961f487ff676c6f96724544ac9bb6e89f373f0e4
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-26 15:34:41 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+    document.querySelector('form').addEventListener('submit', handleEvent)  
+}
+
+const handleEvent = (e) => {
+    e.preventDefault()
+    let input = document.querySelector('#searchByID')
+    // input.value) = (e.target.children[1].value) = e.target.searchById.value
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(json => {
+        const title = document.querySelector("section h4")
+        const summary = document.querySelector('section p')
+        title.textContent = json.title
+        summary.textContent = json.summary
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
